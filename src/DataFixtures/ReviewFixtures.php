675e778bf430bd6d414184d0419b8edb3c846b14
AlanV. <scoutilepudou@gmail.com>
2024-01-08 16:23:47 +0100

    Feat #63: create fixture for review

---
+++ src/DataFixtures/ReviewFixtures.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\DataFixtures\Rental\RentalFixtures;
+use App\Entity\Agency;
+use App\Entity\Review;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class ReviewFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        $review = new Review();
+
+        $review
+            ->setCustomer($this->isInstanceOfUser(UserFixtures::CUSTOMER_REF))
+            ->setAgency($this->isInstanceOfAgency(AgencyFixtures::AGENCY_MARSEILLE_REFERENCE))
+            ->setStar('4.5')
+            ->setDetails('La meilleure agence de Marseille !')
+        ;
+
+        $manager->persist($review);
+        $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            RentalFixtures::class,
+        ];
+    }
+
+    private function isInstanceOfAgency(string $ref): Agency
+    {
+        $ref = $this->getReference($ref);
+
+        return $ref instanceof Agency ? $ref : new Agency();
+    }
+
+    private function isInstanceOfUser(string $ref): User
+    {
+        $ref = $this->getReference($ref);
+
+        return $ref instanceof User ? $ref : new User();
+    }
+}
