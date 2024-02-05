ca8200ca25723430a1ca51e4da51f99062ca838b
Alan <62551143+alan-vle@users.noreply.github.com>
2024-02-05 11:57:05 +0100

    Feat #47: enable and configure api resource on delivery entity (#137)

--- src/Entity/Rental/Delivery.php
+++ src/Entity/Rental/Delivery.php
@@ -2,6 +2,11 @@
 
 namespace App\Entity\Rental;
 
+use ApiPlatform\Metadata\ApiProperty;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
 use App\Entity\Enum\Rental\DeliveryStatusEnum;
 use App\Entity\Trait\TimeStampTrait;
 use App\Entity\Trait\UuidTrait;
@@ -9,22 +14,34 @@ use App\Repository\Rental\DeliveryRepository;
 use App\Service\Utils\EnumUtils;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Attribute\Groups;
 use Symfony\Component\Uid\Uuid;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: DeliveryRepository::class)]
 #[ORM\Table(name: '`delivery`')]
+#[ApiResource(
+    normalizationContext: ['groups' => ['delivery:read', 'identifier', 'timestamp']],
+)]
+#[GetCollection(security: "is_granted('ROLE_ADMIN')")]
+#[Get(
+    security: "is_granted('ROLE_ADMIN') or ".
+    '(object.getRental() and object.getRental().getEmployee().getAgency().getDirector() == user or '.
+    'object.getRental().getEmployee() == user or object.getRental().getCustomer() == user)'
+)]
+#[Delete(security: "is_granted('ROLE_ADMIN')")]
 #[ORM\HasLifecycleCallbacks]
 class Delivery
 {
     use UuidTrait;
     use TimeStampTrait;
 
+    #[ApiProperty(identifier: false)]
     #[ORM\Id, ORM\GeneratedValue, ORM\Column]
     private ?int $id = null;
 
     #[Assert\Type(type: 'boolean', message: 'The value {{ value }} is not a valid {{ type }}.')]
-    #[Assert\NotNull]
+    #[Groups(['delivery:read'])]
     #[ORM\Column(type: types::SMALLINT, enumType: DeliveryStatusEnum::class)]
     private ?DeliveryStatusEnum $status = null;
 
@@ -32,19 +49,24 @@ class Delivery
     #[Assert\NotBlank(message: 'The address should not be blank.')]
     #[Assert\Regex(pattern: '/^[a-zA-Z0-9\s\-\',]*$/', message: 'The {{ value }} is not a valid address.')]
     #[Assert\Length(max: 130, maxMessage: 'The address cannot be longer than {{ limit }} characters')]
+    #[Groups(['delivery:read', 'rental:write'])]
     #[ORM\Column(length: 130)]
     private ?string $address = null;
 
     #[Assert\DateTime]
     #[Assert\NotBlank]
+    #[Groups(['delivery:read', 'rental:write'])]
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
     private ?\DateTimeInterface $deliveryDate = null;
 
-    #[ORM\OneToOne(inversedBy: 'delivery', cascade: ['persist', 'remove'])]
+    #[ApiProperty(readableLink: false, writableLink: false)]
+    #[Groups(['delivery:read'])]
+    #[ORM\OneToOne(inversedBy: 'delivery')]
     #[ORM\JoinColumn(nullable: false)]
     private ?Rental $rental = null;
 
-    #[ORM\OneToOne(inversedBy: 'delivery', cascade: ['persist', 'remove'])]
+    #[ApiProperty(readableLink: false, writableLink: false)]
+    #[ORM\OneToOne(inversedBy: 'delivery')]
     #[ORM\JoinColumn(nullable: true)]
     private ?RentalArchived $rentalArchived = null;
 
